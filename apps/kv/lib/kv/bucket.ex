771566bd34d919aa8fcb68eafefcc4241357064b
hoang <giahoangth@gmail.com>
2021-02-27 14:53:49 +0700

    chore: add docs

--- apps/kv/lib/kv/bucket.ex
+++ apps/kv/lib/kv/bucket.ex
@@ -1,12 +1,22 @@
 defmodule KV.Bucket do
-  def start_link(_content) do
+  use Agent
+  @doc """
+    Starts a new bucket.
+  """
+  def start_link(_) do
     Agent.start_link fn -> %{} end
   end
 
+  @doc """
+  Gets a value from the `bucket` by `key`.
+  """
   def get(agent, key) do
     Agent.get(agent, &Map.get(&1, key))
   end
 
+  @doc """
+  Puts the value for the given `key` in the `bucket`
+  """
   def put(agent, key , value) do
     Agent.update(agent, &Map.put(&1, key, value))
   end
